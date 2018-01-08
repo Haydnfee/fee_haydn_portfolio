b7a119d7a34f86945e607c18468c33ea785dc107
haydnfee <hfeegdci@gmail.com>
2018-01-07 19:33:05 -0500

    added images and uploading current php code

--- js/app.js
+++ js/app.js
@@ -20,6 +20,64 @@ $(document).foundation()
     });
 
 //js
-    (function(){
+    //lightbox
+    var lightbox = document.querySelector('#lightbox')
+        img01 = document.querySelector('#imgCon01')
+        img02 = document.querySelector('#imgCon02')
+        img03 = document.querySelector('#imgCon03')
+        img04 = document.querySelector('#imgCon04')
+        img05 = document.querySelector('#imgCon05')
+        img06 = document.querySelector('#imgCon06')
+        span = document.querySelector('.closeBTN')
+        lbImg = document.querySelector('.lbContent')    
+    
+
+    img01.onclick = (function(){
+        lightbox.style.display = "block";
+        var galImg = window.getComputedStyle(this, null).getPropertyValue("background-Image");
+        lbImg.src = galImg.slice(4, -1).replace(/"/g, "");
+        console.log(galImg);
+    })
+
+    img02.onclick = (function(){
+        lightbox.style.display = "block";
+        var galImg = window.getComputedStyle(this, null).getPropertyValue("background-Image");
+        lbImg.src = galImg.slice(4, -1).replace(/"/g, "");
+        console.log(galImg);
+    })
+
+    img03.onclick = (function(){
+        lightbox.style.display = "block";
+        var galImg = window.getComputedStyle(this, null).getPropertyValue("background-Image");
+        lbImg.src = galImg.slice(4, -1).replace(/"/g, "");
+        console.log(galImg);
+    })
+
+    img04.onclick = (function(){
+        lightbox.style.display = "block";
+        var galImg = window.getComputedStyle(this, null).getPropertyValue("background-Image");
+        lbImg.src = galImg.slice(4, -1).replace(/"/g, "");
+        console.log(galImg);
+    })
+
+    img05.onclick = (function(){
+        lightbox.style.display = "block";
+        var galImg = window.getComputedStyle(this, null).getPropertyValue("background-Image");
+        lbImg.src = galImg.slice(4, -1).replace(/"/g, "");
+        console.log(galImg);
+    })
+
+    img06.onclick = (function(){
+        lightbox.style.display = "block";
+        var galImg = window.getComputedStyle(this, null).getPropertyValue("background-Image");
+        lbImg.src = galImg.slice(4, -1).replace(/"/g, "");
+        console.log(galImg);
+    })
+
+
+
+    span.onclick = (function(){
+        lightbox.style.display = "none";
+
         
-    });
+    }); 
